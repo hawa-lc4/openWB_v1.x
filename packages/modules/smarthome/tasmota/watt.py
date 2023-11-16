63db752328af5bf1d3fe4b9e5707f9f120c5feff
hawa-lc4 <124808307+hawa-lc4@users.noreply.github.com>
2023-11-16 18:38:16 +0100

    changes for complex tasmota environment

--- packages/modules/smarthome/tasmota/watt.py
+++ packages/modules/smarthome/tasmota/watt.py
@@ -18,12 +18,15 @@ except Exception:
 answer = json.loads(str(urllib.request.urlopen("http://"+str(ipadr) +
                     "/cm?cmnd=Status%208", timeout=3).read().decode("utf-8")))
 try:
-    aktpower = int(answer['StatusSNS']['ENERGY']['Power'])
+    aktpower = int(answer['StatusSNS']['ANALOG']['CTEnergy']['Power'])
 except Exception:
     aktpower = 0
+try:
+    powerc = int(answer['StatusSNS']['ANALOG']['CTEnergy']['Energy'])
+except Exception:
+    powerc = 0
 if (aktpower > 50) or (r_status == 1):
     relais = 1
-powerc = 0
 answer = '{"power":' + str(aktpower) + ',"powerc":' + str(powerc) + ',"on":' + str(relais) + '} '
 f1 = open('/var/www/html/openWB/ramdisk/smarthome_device_ret' + str(devicenumber), 'w')
 json.dump(answer, f1)
