471ebf475540a4097b9acfa1e48fa1d01a1cc524
hawa-lc4 <124808307+hawa-lc4@users.noreply.github.com>
2024-09-17 20:42:15 +0200

    Einbindung Heidelberg WB Energy Control mittels ModBusTCP durch Umbau der Vorlage TWCmanager

--- modules/twcmanagerlp1/main.sh
+++ modules/twcmanagerlp1/main.sh
@@ -1,52 +1,57 @@
 #!/bin/bash
-re='^[-+]?[0-9]+\.?[0-9]*$'
 
-if [[ $twcmanagerlp1httpcontrol -eq 1 ]]; then
-	slave=$(curl --connect-timeout 3 -s "http://$twcmanagerlp1ip:$twcmanagerlp1port/api/getSlaveTWCs")
-	status=$(curl --connect-timeout 3 -s "http://$twcmanagerlp1ip:$twcmanagerlp1port/api/getStatus")
-	amps=$(echo "$slave" | jq 'first(.[].reportedAmpsActual)' | sed -e 's/^"//' -e 's/"$//')
-	watts=$(echo "$status" | jq .chargerLoadWatts | sed -e 's/^"//' -e 's/"$//')
-	watt=$(echo "scale=0;$watts" | bc | sed 's/\..*$//')
+wbeclp2ip=$twcmanagerlp2ip
+wbeclp2port=$twcmanagerlp2port
+# wbeclp2ip='192.168.183.114'
+# wbeclp2port=502
+rekwh='^[-+]?[0-9]+\.?[0-9]*$'
 
-	soc=$(echo "$slave" | jq 'first(.[].lastBatterySOC)')
-	volt1=$(echo "$slave" | jq 'first(.[].voltsPhaseA)')
-	volt2=$(echo "$slave" | jq 'first(.[].voltsPhaseB)')
-	volt3=$(echo "$slave" | jq 'first(.[].voltsPhaseC)')
-	kwh_total=$(echo "$slave" | jq '.total.lifetimekWh')
+n=0
+output=$(sudo python3 /var/www/html/openWB/modules/twcmanagerlp2/readwbec.py $wbeclp2ip $wbeclp2port)
 
-	echo $volt1 > /var/www/html/openWB/ramdisk/llv1
-	echo $volt2 > /var/www/html/openWB/ramdisk/llv2
-	echo $volt3 > /var/www/html/openWB/ramdisk/llv3
-	echo $kwh_total > /var/www/html/openWB/ramdisk/llkwh
+while read -r line; do
+        if (( $n == 0 )); then
+                case "$line" in
+                        4 | 5)
+                                echo 1 > /var/www/html/openWB/ramdisk/plugstat
+                                echo 0 > /var/www/html/openWB/ramdisk/chargestat
+                                ;;
+                        6 | 7)
+                                echo 1 > /var/www/html/openWB/ramdisk/plugstat
+                                echo 1 > /var/www/html/openWB/ramdisk/chargestat
+                                ;;
+                        *)
+                                echo 0 > /var/www/html/openWB/ramdisk/plugstat
+                                echo 0 > /var/www/html/openWB/ramdisk/chargestat
+                                ;;
+                esac
+        if (( $n == 1 )); then
+                echo "$line" > /var/www/html/openWB/ramdisk/lla1
+        fi
+        if (( $n == 2 )); then
+                echo "$line" > /var/www/html/openWB/ramdisk/lla2
+        fi
+        if (( $n == 3 )); then
+                echo "$line" > /var/www/html/openWB/ramdisk/lla3
+        fi
+        if (( $n == 4 )); then
+                echo "$line" > /var/www/html/openWB/ramdisk/llv1
+        fi
+        if (( $n == 5 )); then
+                echo "$line" > /var/www/html/openWB/ramdisk/llv2
+        fi
+        if (( $n == 6 )); then
+                echo "$line" > /var/www/html/openWB/ramdisk/llv3
+        fi
+        if (( $n == 7 )); then
+                echo "$line" > /var/www/html/openWB/ramdisk/llaktuell
+        fi
+        if (( $n == 8 )); then
+                llkwhs1=$(echo "$line")
+                if [[ $llkwhs1 =~ $rekwh ]]; then
+                        echo $llkwhs1 > /var/www/html/openWB/ramdisk/llkwh
+                fi
+        fi
 
-	if [[ $watt -lt 4000 ]]; then
-		twcmanagerlp1phasen=1
-	elif [[ $watt -lt 8000 ]]; then
-		twcmanagerlp1phasen=2
-	else
-		twcmanagerlp1phasen=3
-	fi
-else
-	amps=$(curl --connect-timeout 3 -s "http://$twcmanagerlp1ip/index.php" | grep Charging | sed 's/^.*\(Charging at.*A\).*$/\1/' | cut -c 13- | tr -d A)
-	watt=$(echo "scale=0;$amps * 230  * $twcmanagerlp1phasen" | bc | sed 's/\..*$//')
-fi
-
-if ! [[ $amps =~ $re ]] ; then
-	amps="0"
-fi
-
-if (( twcmanagerlp1phasen == 1 )); then
-	echo $amps > /var/www/html/openWB/ramdisk/lla1
-	echo 0 > /var/www/html/openWB/ramdisk/lla2
-	echo 0 > /var/www/html/openWB/ramdisk/lla3
-elif (( twcmanagerlp1phasen == 2 )); then
-	echo $amps > /var/www/html/openWB/ramdisk/lla1
-	echo $amps > /var/www/html/openWB/ramdisk/lla2
-	echo 0 > /var/www/html/openWB/ramdisk/lla3
-elif (( twcmanagerlp1phasen == 3 )); then
-	echo $amps > /var/www/html/openWB/ramdisk/lla1
-	echo $amps > /var/www/html/openWB/ramdisk/lla2
-	echo $amps > /var/www/html/openWB/ramdisk/lla3
-fi
-
-echo $watt > /var/www/html/openWB/ramdisk/llaktuell
+        n=$((n + 1))
+done <<< "$output"
---
+++ modules/twcmanagerlp2/atrebootwbec.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+#from pymodbus.transaction import ModbusRtuFramer
+from pymodbus.client.sync import ModbusTcpClient
+import sys
+
+SERVER_HOST = str(sys.argv[1])
+SERVER_PORT = int(sys.argv[2])
+unit_id = 1
+
+#client = ModbusTcpClient(SERVER_HOST, SERVER_PORT, framer=ModbusRtuFramer)
+client = ModbusTcpClient(SERVER_HOST, SERVER_PORT)
+
+resp = client.read_input_registers(5, 14, unit=unit_id)
+resp = client.read_holding_registers(257, 6, unit=unit_id)
+resp = client.write_register(257, 60000, unit=unit_id)
+resp = client.write_register(258, 4, unit=unit_id)
+resp = client.write_register(262, 100, unit=unit_id)
