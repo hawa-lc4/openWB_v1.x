af10193ffd256706f65eac7d24e0cc0d8ba76f88
hawa-lc4 <124808307+hawa-lc4@users.noreply.github.com>
2026-07-26 19:37:12 +0200

    Update atreboot.sh
    fix invalid command for BPI-M2 to get ipaddress

--- runs/atreboot.sh
+++ runs/atreboot.sh
@@ -322,7 +322,8 @@ at_reboot() {
 	"$OPENWBBASEDIR/runs/services.sh" restart
 
 	# get local ip
-	ip route get 1 | awk '{print $7;exit}' >"$OPENWBBASEDIR/ramdisk/ipaddress"
+	#ip route get 1 | awk '{print $7;exit}' >"$OPENWBBASEDIR/ramdisk/ipaddress"
+	ip route | head -1 | awk '{print $9;exit}' >"$RAMDISKDIR/ipaddress"
 
 	# update our local version
 	sudo git -C "$OPENWBBASEDIR" show --pretty='format:%ci [%h]' | head -n1 >"$OPENWBBASEDIR/web/lastcommit"
