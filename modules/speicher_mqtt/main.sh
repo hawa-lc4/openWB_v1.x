e2f8be8c83373eaa217b02afce8bc3eaceea7f0f
hawa-lc4 <124808307+hawa-lc4@users.noreply.github.com>
2026-08-02 19:10:34 +0200

    Update main.sh
    comment added to show how connection works and which MQTT topics are used.

--- modules/speicher_mqtt/main.sh
+++ modules/speicher_mqtt/main.sh
@@ -5,3 +5,10 @@ RAMDISKDIR="$OPENWBBASEDIR/ramdisk"
 MODULEDIR=$(cd `dirname $0` && pwd)
 
 openwbModulePublishState "BAT" 0 "Kein Fehler"
+
+
+# Per MQTT zu schreiben:
+# openWB/set/houseBattery/W Speicherleistung in Watt, int, positiv Ladung, negativ Entladung
+# openWB/set/houseBattery/WhImported Geladene Energie in Wh, float, nur positiv
+# openWB/set/houseBattery/WhExported Entladene Energie in Wh, float, nur positiv
+# openWB/set/houseBattery/%Soc Ladestand des Speichers, int, 0-100 
