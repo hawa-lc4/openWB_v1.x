1cbf741789202ce11cae64bb1fc943ac7a896d36
hawa-lc4 <124808307+hawa-lc4@users.noreply.github.com>
2026-08-12 23:24:44 +0200

    Problem: WBEC Standby oder nicht; noch nicht gelöst!

--- modules/wbeclp1/atrebootwbec.py
+++ modules/wbeclp1/atrebootwbec.py
@@ -11,10 +11,14 @@ UNIT_ID = int(sys.argv[3])
 client = ModbusTcpClient(SERVER_HOST, SERVER_PORT)
 
 # Register 257: ModBus-Master WatchDog Timeout in ms
-# Register 258: Standby Function Control (0 = enabled; 4 = disabled)
-# Register 262: Standby Function Control im 0,1 A
+# Register 258: Standby Function Control (0 = enabled; 4 = disabled); active about 13 minutes after Ev is unplugged
+# ACHTUNG: wenn Standby deaktiviert ist wird auch der aktuelle Zählerstand nicht in EEPROM
+# geschrieben und geht damit bei Spannungsausfall verloren.
+# Andererseits hat ein extrem häufiger Wechsel in den Standby einen hohen Verschleiß des EEPROM zur Folge.
+# Standby bleibt disabled bis eine brauchbare Lösung zum automatischen Aufwecken der WBEC vorhanden ist.
+# Register 262: Offline Current Control im 0.1 A
 resp = client.read_input_registers(5, 14, unit=UNIT_ID)
 resp = client.read_holding_registers(257, 6, unit=UNIT_ID)
 resp = client.write_register(257, 60000, unit=UNIT_ID)
-resp = client.write_register(258, 0, unit=UNIT_ID)
+resp = client.write_register(258, 4, unit=UNIT_ID)
 resp = client.write_register(262, 110, unit=UNIT_ID)
